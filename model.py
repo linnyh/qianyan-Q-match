8ea657ce65be8c5cd31b1d7a6a919b71eda83e75
Liam <lyhdxzb@icloud.com>
2021-09-30 21:41:58 +0800

    添加一个LSTM编码token级别嵌入，与句子级别嵌入融合后输入分类器

--- model.py
+++ model.py
@@ -24,9 +24,9 @@ class QuestionMatching(nn.Layer):
         super().__init__()
         self.ptm = pretrained_model  # 预训练模型
         self.dropout = nn.Dropout(dropout if dropout is not None else 0.1)
-
+        self.lstm = MatchLSTM(self.ptm.config["hidden_size"], self.ptm.config["hidden_size"], 1)
         # num_labels = 2 (similar or dissimilar)
-        self.classifier = nn.Linear(self.ptm.config["hidden_size"], 2)  # 线性分类层
+        self.classifier = nn.Linear(self.ptm.config["hidden_size"] * 2, 2)  # 线性分类层
         self.rdrop_coef = rdrop_coef
         self.rdrop_loss = ppnlp.losses.RDropLoss()
 
@@ -44,12 +44,17 @@ class QuestionMatching(nn.Layer):
             将 encoder_output （各个token的嵌入表示） 作为输入
             在bert后上添加自己的东西
         '''
+        output, final_states = self.lstm(encoder_output)
+        # print(final_states[0])
+        final_state = paddle.squeeze(x=final_states[0], axis=0)
+        final = paddle.concat(x=[cls_embedding1, final_state], axis=1)
+        print(final.shape)
 
-        cls_embedding1 = self.dropout(cls_embedding1)  # [32,768]
+        final = self.dropout(final)  # [32,768]
         '''
             分类层改进
         '''
-        logits1 = self.classifier(cls_embedding1)  # [batch_size, 2] 二分类
+        logits1 = self.classifier(final)  # [batch_size, 2] 二分类
 
         # For more information about R-drop please refer to this paper: https://arxiv.org/abs/2106.14448
         # Original implementation please refer to this code: https://github.com/dropreg/R-Drop
@@ -66,13 +71,18 @@ class QuestionMatching(nn.Layer):
 
 
 class MatchLSTM(nn.Layer):
-    def __init__(self):
-        pass
+    def __init__(self, input_size, hidden_size, num_layers):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, time_major=False)
+
+    def forward(self, x):
+        x = self.lstm(x)
+        return x
 
 
 class CrossAttention(nn.Layer):
     def __init__(self):
-        self.rnn = nn.LSTM
+        self.rnn = nn.LSTM()
 
     def forward(self, *inputs, **kwargs):
         pass
